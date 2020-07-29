d02a7cd5c7bafe012ff7884f97f8d1bd6fbc1764
Aaron Lain <aaron.j.lain@gmail.com>
2020-07-28 21:08:11 -0500

    now you can select operation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,9 +8,11 @@ namespace Calculator
     {
         private static void Operator(string numbers)
         {
-            string[] numbs = numbers.Split(",");
+            char op = numbers[0]; // save the operator to determine type of operation to perform
+            string[] numbs = numbers.Split(","); 
+            numbs = numbs.Skip(1).ToArray(); //remove operator so you don't break the type conversions
             int[] nums = numbs.Select(int.Parse).ToArray();
-            List<double> powers = new List<double>();
+            List<double> powers = new List<double>(); //List is variable in size vs. array. also, Math.Pow saves as double so make sure type is correct
 
             int sum = nums.Sum();
             int prod = 1;
@@ -26,9 +28,26 @@ namespace Calculator
 
             string powerList = String.Join(", ", powers);
 
-            Console.WriteLine($"The sum of your numbers is: {sum}");
-            Console.WriteLine($"The product of your numbers is: {prod}");
-            Console.WriteLine($"Your numbers raised to the second power: {powerList}");
+            switch (op)
+            {
+                case '*':
+                    Console.WriteLine($"The product of your numbers is: {prod}");
+                    break;
+                case '+':
+                    Console.WriteLine($"The sum of your numbers is: {sum}");
+                    break;
+                case '^':
+                    Console.WriteLine($"Your numbers raised to the second power: {powerList}");
+                    break;
+                default:
+                    Console.WriteLine($"The sum of your numbers is: {sum}");
+                    Console.WriteLine($"The product of your numbers is: {prod}");
+                    Console.WriteLine($"Your numbers raised to the second power: {powerList}");
+                    break;
+
+            }
+            
+            
 
         }
         static void Main(string[] args)
@@ -36,6 +55,7 @@ namespace Calculator
             Console.WriteLine("Enter the numbers you would like to multiply:");
             var input = Console.ReadLine();
             Operator(input);
+
         }
     }
 }
